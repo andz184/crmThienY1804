09beecfb72198ca22dbee17673d492fb0acfc14c
andz184 <anhandepgiai22@gmail.com>
2025-06-09 03:10:50 +0700

    âdadadad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/adminlte-custom-dark.css') }}">
     @stack('styles')
 </head>
 <body>
